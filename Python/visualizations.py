258a78806b14eb73fedb7bee06f83ccfde332440
Vedanth V Baliga <vedanthvbaliga@gmail.com>
2023-03-13 15:24:15 +0530

    update : update the path to air quality uci dataset

--- Python/visualizations.py
+++ Python/visualizations.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("C:\Users\student.dsu.000\Desktop\vb\AirQualityUCI.csv")
+df = pd.read_xlsx("https://github.com/vedanthv/data-science-II-lab/blob/main/AirQualityUCI.xlsx")
 
 # Boxplot
 plt.boxplot(df['T'])
